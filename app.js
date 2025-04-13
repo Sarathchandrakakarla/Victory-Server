ecc6301365f20da7e5f7dbe3abef82ea656208bc
Sarathchandra Reddy Kakarla <kakarlasarath4@gmail.com>
2025-04-13 09:58:27 +0530

    Activity Bug Fixed on 13-04-2025

--- app.js
+++ app.js
@@ -286,7 +286,7 @@ app.post("/student_login", (req, res) => {
                   message: "Your Login has been Disabled..Contact Admin Office",
                 });
               }
-              /* logger.info({
+              logger.info({
                 label: "Authentication",
                 message: {
                   user: "Student",
@@ -297,7 +297,7 @@ app.post("/student_login", (req, res) => {
                 timestamp: new Date().toLocaleString(undefined, {
                   timeZone: "Asia/Kolkata",
                 }),
-              }); */
+              });
               res.json({
                 success: true,
                 data: { Name: rows[0].First_Name },
